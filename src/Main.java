92c6214257a9f6122d84dd29b17d0f2b06572ec1
Orlandopc97 <orlandopc1797@gmail.com>
2024-11-22 14:26:01 -0600

    Archivos if-else

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int edad = 27;
+        //== igual
+        //!= es distinto (diferente)6
+        //< menor
+        //> mayor
+        //>=
+        //<=
+        if (edad > 18){
+            System.out.println("Puedes entrar a la disco sin problemas");
+        }else if(edad == 18) {
+            System.out.println("puedes entrar pero enseña tu ine");
+        }else {
+            System.out.println("no puede entrar a las disco, edad insuficiente");
+
+        }
+
+
+
+
+    }
+}
